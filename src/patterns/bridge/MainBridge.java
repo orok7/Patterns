e686da2fb5a7998724f9ae9515a17a3ef9878b4f
orok7 <orestmykytyn@gmail.com>
2017-10-14 22:41:28 +0300

    Bridge added

--- src/patterns/bridge/MainBridge.java
+++ src/patterns/bridge/MainBridge.java
@@ -3,7 +3,11 @@ package patterns.bridge;
 public class MainBridge {
 
 	public static void main(String[] args) {
+		Shape redCircle = new Circle(100, 100, 10, new RedCircle());
+		Shape greenCircle = new Circle(100, 100, 10, new GreenCircle());
 
+		redCircle.draw();
+		greenCircle.draw();
 	}
 
 }
---
+++ src/patterns/bridge/Shape.java
@@ -0,0 +1,11 @@
+package patterns.bridge;
+
+public abstract class Shape {
+	protected DrawAPI drawAPI;
+
+	protected Shape(DrawAPI drawAPI) {
+		this.drawAPI = drawAPI;
+	}
+
+	public abstract void draw();
+}
