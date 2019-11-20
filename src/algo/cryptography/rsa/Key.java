d97d586d7aa4c0413b2a87aa2744d103c83b8e4e
orok7 <orestmykytyn@gmail.com>
2019-11-20 17:07:06 +0200

    RSA // has a not included dep

---
+++ src/algo/cryptography/rsa/Key.java
@@ -0,0 +1,20 @@
+package algo.cryptography.rsa;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Key {
+    private long firstParam;
+    private long secondParam;
+
+    @Override
+    public String toString() {
+        return "Key{" + "firstParam=" + firstParam + ", secondParam=" + secondParam + '}';
+    }
+}
---
+++ src/algo/cryptography/rsa/RsaCipher.java
@@ -0,0 +1,62 @@
+package algo.cryptography.rsa;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.math.BigInteger;
+
+import static algo.cryptography.rsa.KeyStringUtils.decodeKey;
+import static algo.cryptography.rsa.KeyStringUtils.decodeStr;
+import static algo.cryptography.rsa.KeyStringUtils.encodeStr;
+import static com.sun.xml.internal.fastinfoset.stax.events.Util.isEmptyString;
+
+public class RsaCipher {
+
+    private static final ObjectMapper mapper = new ObjectMapper();
+
+    public static String encode(String msg, String publicKey) {
+        if (isEmptyString(msg)) {
+            throw new IllegalArgumentException("Empty message.");
+        }
+        Key key = decodeKey(publicKey);
+        BigInteger n = new BigInteger(String.valueOf(key.getFirstParam()));
+        BigInteger e = new BigInteger(String.valueOf(key.getSecondParam()));
+        int msgLength = msg.length();
+        long[] result = new long[msgLength];
+        for (int i = 0; i < msgLength; i++) {
+            char c = msg.charAt(i);
+            BigInteger m = new BigInteger(Integer.valueOf(c).toString());
+            result[i] = m.modPow(e, n).longValue();
+        }
+        try {
+            return encodeStr(mapper.writeValueAsString(result));
+        } catch (JsonProcessingException ex) {
+            ex.printStackTrace();
+        }
+        return null;
+    }
+
+    public static String decode(String msg, String privateKey) {
+        if (isEmptyString(msg)) {
+            throw new IllegalArgumentException("Empty message.");
+        }
+        Key key = decodeKey(privateKey);
+        BigInteger n = new BigInteger(String.valueOf(key.getFirstParam()));
+        BigInteger d = new BigInteger(String.valueOf(key.getSecondParam()));
+        long[] encoded;
+        try {
+            encoded = mapper.readValue(decodeStr(msg), long[].class);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+        StringBuilder result = new StringBuilder();
+        for (long l : encoded) {
+            BigInteger c = new BigInteger(Long.valueOf(l).toString());
+            result.append((char) c.modPow(d, n).intValue());
+        }
+        return result.toString();
+    }
+
+}
